af10e26667b85bdf19e4d4a562cfb490695735c7
epeters95 <ewpeters@asu.edu>
2016-07-03 15:01:30 -0700

    Everything up to demo phase

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   
   root to: 'static_pages#index'
 
-  # scope :static_pages do
-  #   get '/index', to: 
-  # end
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/contact', to: 'static_pages#contact', as: 'contact'
+  get '/electro_mech', to: 'static_pages#electro_mech', as: 'electro_mech'
+  get '/servo', to: 'static_pages#servo', as: 'servo'
+  get '/vfd', to: 'static_pages#vfd', as: 'vfd'
+  get '/motors', to: 'static_pages#motors', as: 'motors'
+  
 
 end
